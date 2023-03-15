829dff8c1281b4490494127414ea219a31795b1f
Penumallu Durga Manikanta Reddy <109668932+PenumalluDurgamanikantaReddy@users.noreply.github.com>
2023-03-16 01:32:41 +0530

    Update People.js

--- src/components/People.js
+++ src/components/People.js
@@ -1,1 +1,51 @@
 
+import React,{useEffect,useState} from "react";
+
+function Peoples() {
+    const [emails,Setemails]=useState([])
+    const [unique,Setunique]=useState([])
+    const [check,Setcheck]=useState(1)
+    let mailarr=[]
+    useEffect(() => {
+  
+        fetch('https://slack-80008-default-rtdb.firebaseio.com/.json')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            for(let mail in data){
+                mailarr.push(data[mail].email)
+                // console.log(data)
+                Setcheck((prevstate)=>{
+                         return prevstate+1 
+                })
+            }
+            Setemails(mailarr)
+            Setunique([...new Set(emails)])
+        }
+      
+            )
+        .catch(error => console.error(error));
+    }, [mailarr])
+
+  return (
+    <div>
+    <div className="mails">
+      <h1>Registered Gmail accounts:</h1>
+      <ul className="emailslist">
+  {
+   
+    unique.map((each,index)=>{
+        return(
+
+            <li key={index}>{each}</li>
+        )
+    })
+    
+  }
+  </ul>
+  </div>
+    </div>
+  );
+}
+
+export default Peoples
